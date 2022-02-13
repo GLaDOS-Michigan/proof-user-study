0221c250f3efeef61e8784d7fd289f2eacbd6f7b
Tony Zhang <nz97@cornell.edu>
2022-02-13 15:26:43 -0500

    use json file for project metadata

--- utils/README.md
+++ utils/README.md
@@ -1,3 +1,16 @@
 * `punch-in [path-to-timecard]` logs a timestamp in the timecard csv file
 * `punch-out [path-to-timecard]` logs a timestamp in the timecard csv file
 * `verify-and-commit -tag=[ "other" | "protocol" | "proof" ] [ commit message ] [files]` commits all files in the repo with the tag and commit message, and then verifies the files in Dafny
+
+
+Each project should contain a json file that includes the following:
+
+```json
+{
+    "project_path" : project root dir,
+    "timecard_path" : path to timecard,
+    "repo_path" : path to git repo,
+    "protocol_files" : list of protocol files,
+    "proof_files" : list of proof files
+}
+```
--- utils/analyze-project.py
+++ utils/analyze-project.py
@@ -1,34 +1,40 @@
 import sys
 import csv
 import datetime
+import json
+from git import Repo
 
-FILETYPES_CSV = "files.csv"
-TIMECARD_CSV = "timecard.csv"
 
-def main(project_dir):
-    files_info = get_files_info(project_dir)            # map of categories to list of filenames    
-    timecard_info = get_timecard_info(project_dir)      # list of (start, end) tuples
-    print(timecard_info)
+def main(project_json):
+    with open(project_json) as f:
+        info = json.load(f)
+        check_well_formed(info)
+    files_info = parse_files_info(info)         # map of categories to list of filenames    
+    timecard_info = parse_timecard_info(info)   # list of (start, end) tuples
+    git_commits = get_git_commits(info)         # list of git commit objects
+    # # visualize_data()
     
+    
+def check_well_formed(info):
+    assert 'project_path' in info
+    assert 'timecard_path' in info
+    assert 'repo_path' in info
+    assert 'protocol_files' in info
+    assert 'proof_files' in info
+        
     
 """ Returns a map of categories to list of filenames """
-def get_files_info(project_dir):
+def parse_files_info(info):
     res = dict()
-    with open("%s/%s" %(project_dir, FILETYPES_CSV)) as f:
-        csvreader = csv.reader(f, delimiter=',')
-        next(csvreader)
-        for row in csvreader:
-            kind, name = row[0], row[1]
-            if kind not in res:
-                res[kind] = []
-            res[kind].append(name)
+    res['protocol'] = info['protocol_files']
+    res['proof'] = info['proof_files']
     return res
 
 
 """ Returns a list of (start, end) tuples """
-def get_timecard_info(project_dir):
+def parse_timecard_info(info):
     res = []
-    with open("%s/%s" %(project_dir, TIMECARD_CSV)) as f:
+    with open(info['timecard_path']) as f:
         csvreader = csv.reader(f, delimiter=',')
         next(csvreader)
         started = False
@@ -48,10 +54,19 @@ def get_timecard_info(project_dir):
     return res 
 
 
+""" Returns list of commit objects limited to project dir """
+def get_git_commits(info):
+    repo = Repo(info['repo_path'])
+    assert not repo.bare
+    commits = list(repo.iter_commits(paths=info['project_path']))
+    print(commits[0].message)
+    return commits
+
+
 if __name__ == "__main__":
-    # positional arguments <project-directory>
+    # positional arguments <project's info.json>
     if len(sys.argv) < 2:
-        print("Error: Expect project directory as input")
+        print("Error: Expect json file as input")
         sys.exit(1)
-    project_dir = sys.argv[1]
-    main(project_dir)
+    project_json = sys.argv[1]
+    main(project_json)
